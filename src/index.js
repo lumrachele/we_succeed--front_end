ac10e9f7f655ef4b24debc1746e13d6464f7dff1
satyasinha94 <satyasinha94@gmail.com>
2019-01-23 11:13:06 -0500

    goal history bar chart

--- src/index.js
+++ src/index.js
@@ -85,61 +85,60 @@ function goalPoints () {
 
 }
   function renderGoals(user){
-    const renderGoalsList = document.querySelector("#goals-list")
+    const reducer = (acc, curr) => acc + curr
 
+    const renderGoalsList = document.querySelector("#goals-list")
     const goalsList = filterGoals(user)
-    const someShit = goalsList.map(function(goal){
+    const goalActivity = goalsList.map(function(goal){
       return goalActivities(user, goal.id)
       })
-      console.log(someShit);
+    const goalNameArray = goalsList.map(goal => goal.name)
+    console.log(goalsList);
+    const goalActpoints = goalActivity.map(function(act) {
+      return act.map(function(act){return act.points}).reduce(reducer, 0)
+    })
+
+    const goalValues = goalsList.map(goal => goal.value)
+    console.log(goalValues)
+    const goalPercentages = goalValues.map(function(value, i) {
+      return (goalActpoints[i] / value) * 100
+    })
+    return `${new Chart(myChart, {
+    type: 'bar',
+    data: {
+      labels: goalNameArray,
+      datasets: [{
+          label: 'total points (%)',
+          data: goalPercentages,
+          backgroundColor: [
+              'rgba(255, 99, 132)',
+              'rgba(54, 162, 235)',
+              'rgba(255, 206, 86)',
+              'rgba(75, 192, 192)',
+              'rgba(153, 102, 255)',
+              'rgba(255, 159, 64)'
+          ],
+          borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+          }]
+      }
     }
-
-
-
-
-
-
-//   function renderGoals (goals) {
-//     debugger
-//     return goals.map(goal => {
-//       return `${new Chart(myChart, {
-//     type: 'bar',
-//     data: {
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132)',
-//                 'rgba(54, 162, 235)',
-//                 'rgba(255, 206, 86)',
-//                 'rgba(75, 192, 192)',
-//                 'rgba(153, 102, 255)',
-//                 'rgba(255, 159, 64)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// })}`
-//     }).join('')
-//   }
-
+    })}`
+    }
 
 });//end of DOMContentLoaded
